533572dec226ad1c374f5174fde741667aa4c720
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-05-15 21:39:04 -0300

    controlADORES

--- App/Route.php
+++ App/Route.php
@@ -7,9 +7,14 @@
             $routes['home'] = array(
                 'route' => '/',
                 'controller' => 'indexController',
+                'action' => 'index'
             );
 
-            $routes['sobre_nos'];
+            $routes['sobre_nos'] = array(
+                'route' => '/',
+                'controller' => 'indexController',
+                'action' => 'sobreNos'
+            );
         }
         public function getUrl(){
             return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
