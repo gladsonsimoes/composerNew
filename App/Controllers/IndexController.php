27d54973a3ab6afc7896c6ac52493e6cf38946a0
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-05-16 19:46:29 -0300

    funcoes da index e sobreNos no indexController

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -1,3 +1,14 @@
 <?php
+  namespace App\Controllers;
+
+  class IndexController{
+    public function index(){
+        echo 'estamos na index';
+    }
+
+    public function  sobreNos(){
+        echo 'estamos na sobreNos';
+    }
+  }
 
 ?>
