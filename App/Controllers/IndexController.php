ef8a1a054e2852c56f508e9959511dfd7c8a3444
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-05-16 21:32:01 -0300

    usando uma funcao render para executar as classes

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -4,11 +4,18 @@
   class IndexController{
     public function index(){
         $dados = array('amora', 'pera', 'manga'); //dados do index
-        require_once("../App/Views/index/index.phtml");
+        $this->render("index" , $dados); //funcao render que irá passar o view e os dados 
     }
 
     public function  sobreNos(){
-        require_once("../App/Views/index/sobreNos.phtml");
+        $dados = array('bala', 'caramelo', 'jujuba'); 
+        $this->render("sobreNos" , $dados);
+    }
+
+
+    //em vez de escrever require_once varias vezes estamos usando uma funcao render
+    public function  render($view, $dados){
+        require_once("../App/Views/index/".$view.".phtml");
     }
   }
 
